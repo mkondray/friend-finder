7aa94eba6a210d514d9e874491478ac4925e2826
mkondray <mkondray@gmail.com>
2018-05-04 20:07:26 -0400

    all files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # friend-finder
+
+Check out the LIVE app [here!](https://git.heroku.com/mk-friend-finder.git)
---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Daryl",
+	"photo" : "http://i0.kym-cdn.com/entries/icons/original/000/002/164/6066631654034590990.jpg",
+	"scores" : [4,4,2,5,1,3,2,5,2,1]
+}, {
+	"name" : "Dan",
+	"photo" : "http://goodnews.ws/wp-content/uploads/2010/03/7995.jpg",
+	"scores" : [5,1,1,1,1,1,1,1,1,5]
+}, {
+	"name" : "Stacy",
+	"photo" : "http://s16749.pcdn.co/wp-content/uploads/2017/12/1001003509-knitters_01.jpg",
+	"scores" : [3,3,3,3,5,4,2,3,3,3]
+}];
+
+
+module.exports = friends;
